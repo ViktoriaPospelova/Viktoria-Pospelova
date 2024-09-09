2dd78638aeec588e714d769f44b989f7d48109d2
ViktoriaPospelova <vavikka@gmail.com>
2024-09-09 23:40:34 +0300

    Delete Viktoria Pospelova/hw2_ex6.py

--- Viktoria Pospelova/hw2_ex6.py
+++
@@ -1,6 +0,0 @@
-#6.	Создать множество из 5 разных чисел, затем добавить в него новое число и вывести на экран
-spis = []
-for i in range(5):
-    print('Vvedite ', i+1, ' element')
-    spis.append(input())
-print(spis)
